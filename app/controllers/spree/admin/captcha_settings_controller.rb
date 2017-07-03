2817151eef1e8ccaa824898fdb94ce245d69b869
Joshua Novak <JoshuaNovak919@gmail.com>
2017-07-03 18:31:52 -0400

    Fixes for Spree 3.2.1

--- app/controllers/spree/admin/captcha_settings_controller.rb
+++ app/controllers/spree/admin/captcha_settings_controller.rb
@@ -2,7 +2,15 @@ module Spree
   module Admin
     class CaptchaSettingsController < BaseController
       def update
-        Spree::Captcha::Config.set(params[:preferences])
+        #Spree::Captcha::Config.set(params[:preferences])
+
+        params[:preferences].each do |name, value|
+          next unless Spree::Captcha::Config.has_preference? name
+
+          Spree::Captcha::Config[name] = value
+        end
+
+        #Spree::Captcha::Config.use_captcha = false
 
         respond_to do |format|
           format.html { redirect_to edit_admin_captcha_settings_path }
